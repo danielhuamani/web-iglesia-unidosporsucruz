31f5eaf8e435a52deb4ac6e6a25c74f247783ea1
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2016-01-26 00:50:13 -0500

    agregando primeras pages

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var jade = require('gulp-jade');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+var swig = require('gulp-swig');
+var connect = require('gulp-connect');
+var historyApiFallback = require('connect-history-api-fallback');
+var directorio = {
+
+  jade: ['app/templates/jade/*.jade'],
+  stylus: ['app/static/stylus/styles.styl']
+
+};
+
+gulp.task('stylus', function () {
+  gulp.src(directorio.stylus)
+    .pipe(plumber())
+    .pipe(stylus({
+      compress: true
+      }))
+    .pipe(gulp.dest('app/static/css/'))
+    .pipe(connect.reload());
+
+});
+var opts = {
+  load_json: true,
+
+  data: {
+    STATIC_URL : '../../static/',
+  }
+};
+
+gulp.task('templates', function() {
+
+    var build_vars = {
+      STATIC_URL:'../../static/'
+    }
+  return gulp.src(directorio.jade)
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true,
+      locals: build_vars
+      }))
+    .pipe(gulp.dest('app/templates/html'))
+    .pipe(connect.reload());
+
+
+});
+
+gulp.task('watch', function() {
+
+	gulp.watch('app/static/stylus/styles.styl', ['stylus']),
+	gulp.watch('app/templates/jade/*.jade', ['templates'])
+});
+
+//creacioon  del server para el livereload
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app/',
+    hostname: '0.0.0.0',
+    livereload: true,
+    port: '3000',
+    open: true,
+    middleware: function(connect, opt) {
+      return [ historyApiFallback() ];
+    }
+  });
+});
+gulp.task('default', ['stylus', 'templates', 'watch', 'connect']);
+//creacioon  del server para el livereload
